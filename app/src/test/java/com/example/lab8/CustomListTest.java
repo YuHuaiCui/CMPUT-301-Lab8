3db0899352bec34b1ec37120896334702682b2ee
Daniel Cui <you@example.com>
2024-10-22 17:32:57 -0600

    simple test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,7 +2,9 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,6 +13,34 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private CustomList mockCustomList() {
+        CustomList customList = new CustomList(null, new ArrayList<City>());
+        customList.addCity(mockCity());
+        return customList;
+    }
 
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
+    }
+
+    @Test
+    void testAdd() {
+        CustomList cityList = mockCustomList();
+        assertEquals(1, cityList.getCities().size());
+        City city = new City("Regina", "Saskatchewan");
+        cityList.add(city);
+        assertEquals(2, cityList.getCities().size());
+        assertTrue(cityList.getCities().contains(city));
+    }
+
+    @Test
+    void testAddException() {
+        CustomList cityList = mockCustomList();
+        City city = new City("Yellowknife", "Northwest Territories");
+        cityList.add(city);
+        assertThrows(IllegalArgumentException.class, () -> {
+            cityList.add(city);
+        });
+    }
 
 }
